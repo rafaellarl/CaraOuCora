3e1f855e2dd67063f8b16aab53df433d7354bcd1
rafaellar <rafaellar.dev@gmail.com>
2025-08-04 08:09:31 -0300

    create layout result screen

--- CaraOuCora/ViewController.swift
+++ CaraOuCora/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  CaraOuCora
-//
-//  Created by RAFAELLA RIBEIRO LIMA BRITTO on 03/08/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
